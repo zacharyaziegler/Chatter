19237aff51b68b817c01886dbf44f0bba7868e87
zacharyaziegler <zacharyaziegler@gmail.com>
2025-03-08 17:25:54 -0500

    Chat feature working

--- backend/src/main/java/com/onlinechatapp/BackendApplication.java
+++ backend/src/main/java/com/onlinechatapp/BackendApplication.java
@@ -2,8 +2,10 @@ package com.onlinechatapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {HibernateJpaAutoConfiguration.class, DataSourceAutoConfiguration.class})
 public class BackendApplication {
 
 	public static void main(String[] args) {
